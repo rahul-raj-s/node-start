0ca99d5b7a6707f55c3a95e5dc515004e008e3b2
rahul raj <rahul.raj@quantuminventions.com>
2020-06-05 00:21:23 +0530

    set up app for heroku

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = process.env.PORT || 8000;
 
 const public_dir = path.join(__dirname, "../public");
 app.use(express.static(public_dir));
 app.set("view engine", "hbs");
+
 // app.get("/help", (req, res) => {
 //   res.send("Helping");
 // });
@@ -16,6 +18,6 @@ app.get("/userlist", (req, res) => {
   res.send([{ name: "Rahul" }, { name: "Dhan" }]);
 });
 
-app.listen(3000, () => {
-  console.log("Listening at port 3000");
+app.listen(port, () => {
+  console.log("Listening at port", port);
 });
